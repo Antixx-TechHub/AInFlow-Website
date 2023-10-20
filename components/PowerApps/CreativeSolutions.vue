5d652245feb22e448696c86f7c5e8897e51e9e47
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-10-20 15:50:44 +0530

    power bi introuduction is added

--- components/PowerApps/CreativeSolutions.vue
+++ components/PowerApps/CreativeSolutions.vue
@@ -4,17 +4,14 @@
             <div class="overview-box">
                 <div class="overview-content">
                     <div class="content">
-                        <span class="sub-title">Digital Marketing</span>
-                        <h2>Creative Solutions, Creative Results</h2>
-                        <p>We believe brand interaction is key in communication. Real innovations and a positive customer experience are the heart of successful communication.</p>
-                        <div class="features-text">
-                            <h4>Core Development</h4>
-                            <p>No fake products and services. The customer is king, their lives and needs are the inspiration.</p>
-                        </div>
-                        <div class="features-text">
-                            <h4>Define Your Choices</h4>
-                            <p>No fake products and services. The customer is king, their lives and needs are the inspiration.</p>
-                        </div>
+                        <span class="sub-title">AinFlow</span>
+                        <h2>Introduction</h2>
+                        <p>Welcome to the world of AInFlow, where innovation meets functionality. In today's fast-paced
+                            business landscape, the ability to adapt and streamline your processes is crucial. Power Apps is
+                            here to help you do just that by allowing you to create custom applications that cater to your
+                            unique needs. Whether you're looking to enhance productivity, improve efficiency, or
+                            revolutionize the way you interact with your data, Power Apps is the solution you've been
+                            waiting for.</p>
                     </div>
                 </div>
 
@@ -31,12 +28,13 @@
                         <img src="~/assets/images/services/services-img2.png" alt="image">
                     </div>
                 </div>
-                
+
                 <div class="overview-content">
                     <div class="content right-content">
                         <span class="sub-title">Social Media Marketing</span>
                         <h2>Solving Problems, Building Brands</h2>
-                        <p>We believe brand interaction is key in communication. Real innovations and a positive customer experience are the heart of successful communication.</p>
+                        <p>We believe brand interaction is key in communication. Real innovations and a positive customer
+                            experience are the heart of successful communication.</p>
                         <ul class="features-list">
                             <li><span><i class='bx bx-check'></i> Recommender Systems</span></li>
                             <li><span><i class='bx bx-check'></i> Demand Prediction</span></li>

ad220a8b3438c59d14426a2fade36e60fed6b921
AntixxTechHub <info@antixxtechhub.com>
2023-11-01 11:50:07 +0530

    new changes

--- components/PowerBI/CoreBusiness.vue
+++ components/PowerBI/CoreBusiness.vue
@@ -92,7 +92,7 @@
                     <div class="col-lg-4 col-md-6 col-sm-6">
                         <div class="single-services-box">
                             <div class="icon">
-                                <i class="flaticon-content"></i>
+                                <img src="~/assets/images/business-analysts.png" alt="image">
                                 <div class="circles-box">
                                     <div class="circle-one"></div>
                                     <div class="circle-two"></div>

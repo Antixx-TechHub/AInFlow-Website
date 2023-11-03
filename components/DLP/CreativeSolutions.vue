02b51229d978eea8695cf377c3d741ae6b442d4b
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-11-03 18:33:50 +0530

    dlp images uploaded

--- components/DLP/CreativeSolutions.vue
+++ components/DLP/CreativeSolutions.vue
@@ -19,7 +19,7 @@
 
                 <div class="overview-image">
                     <div class="image">
-                        <img src="~/assets/images/services/services-img1.png" alt="image">
+                        <img src="~/assets/images/dlp/WhatisDLP.png" alt="image">
                     </div>
                 </div>
             </div>
